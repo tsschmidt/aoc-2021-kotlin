e83d4faac04459256d5979698767498c91b1ad24
Travis Schmidt <travis.schmidt@gmail.com>
2021-12-04 09:23:31 -0800

    Day 4 solution

--- src/day2/Day02.kt
+++ src/day2/Day02.kt
@@ -4,38 +4,38 @@ import readInput
 
 fun main() {
     fun part1(input: List<Pair<String, Int>>): Int {
-        var hpos = 0;
-        var dpos = 0;
-        input.forEach {
-            when(it.first) {
-                "forward" -> hpos += it.second
-                "down" -> dpos += it.second
-                "up" -> dpos -= it.second
+        var hpos = 0
+        var dpos = 0
+        input.forEach { (command, amount) ->
+            when(command) {
+                "forward" -> hpos += amount
+                "down" -> dpos += amount
+                "up" -> dpos -= amount
             }
         }
         return hpos * dpos
     }
 
     fun part2(input: List<Pair<String, Int>>): Int {
-        var hpos = 0;
-        var dpos = 0;
-        var aim = 0;
-        input.forEach {
-            when(it.first) {
+        var hpos = 0
+        var dpos = 0
+        var aim = 0
+        input.forEach { (command, amount) ->
+            when(command) {
                 "forward" -> {
-                    hpos += it.second
-                    dpos += aim * it.second
+                    hpos += amount
+                    dpos += aim * amount
                 }
-                "down" -> aim += it.second
-                "up" -> aim -= it.second
+                "down" -> aim += amount
+                "up" -> aim -= amount
             }
         }
         return hpos * dpos
     }
 
-    val input = readInput("day2/test")
+    val input = readInput("day2/data")
         .map { it.split(" ") }
-        .map { Pair(it.get(0), it.get(1).toInt()) }
+        .map { (a, b) -> Pair(a, b.toInt()) }
     println(input)
     println(part2(input))
 }
---
+++ src/day4/Day04.kt
@@ -0,0 +1,96 @@
+package day4
+
+import readInput
+
+fun main() {
+
+    fun getCards(input: List<String>): MutableList<Card> {
+        var cells = mutableListOf<List<Cell>>()
+        val cards = mutableListOf<Card>()
+        for (i in 2 until input.size) {
+            if (input[i].isNotEmpty()) {
+                cells.add(input[i].windowed(3, 3, true).map { Cell(it.trim().toInt()) } )
+            } else {
+                cards.add(Card(cards.size, cells.toList()))
+                cells = mutableListOf()
+            }
+        }
+        cards.add(Card(cards.size, cells.toList()))
+        return cards
+    }
+
+    fun part1(draws: MutableList<Int>, cards: MutableList<Card>): Int {
+        var winner: Card? = null
+        var d: Int? = null
+        while (winner == null) {
+            d = draws.removeFirst()
+            cards.forEach { it.mark(d) }
+            winner = cards.firstOrNull { it.checkForBingo() }
+        }
+        return winner.sumCard() * d!!
+    }
+
+    fun part2(draws: MutableList<Int>, cards: MutableList<Card>): Int {
+        var winners: List<Card>? = null
+        var dWinner: Int? = null
+        while (cards.size > 0 && draws.size > 0) {
+            val d = draws.removeFirst()
+            cards.forEach { it.mark(d) }
+            val curWinners = cards.filter { it.checkForBingo() }
+            if (curWinners.isNotEmpty()) {
+                winners = curWinners
+                cards.removeAll(curWinners)
+                dWinner = d
+            }
+        }
+        return winners!![0].sumCard() * dWinner!!
+    }
+
+    val input = readInput("day4/data")
+    println(part2(input[0].split(",").map { it.toInt() }.toMutableList(), getCards(input)))
+}
+
+data class Cell(val id: Int, var marked: Boolean = false)
+
+data class Card(val id: Int, val cells: List<List<Cell>>) {
+
+    fun mark (drawn: Int) {
+        cells.flatten().firstOrNull { c -> c.id == drawn }?.let { it.marked = true }
+    }
+
+    fun cols(): List<List<Cell>> {
+        val cols = mutableListOf<MutableList<Cell>>()
+        for(i in 0 until 5) {
+            cols.add(mutableListOf())
+            for (j in 0 until 5) {
+                cols[i].add(cells[j][i])
+            }
+        }
+        return cols
+    }
+
+    fun checkForBingo(): Boolean {
+        return cells.any { it.all { c -> c.marked } } ||
+            cols().any { it.all { c -> c.marked } }
+    }
+
+    fun sumCard(): Int {
+        return cells.flatten().filter { c -> !c.marked }.sumOf { c -> c.id }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Card
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id
+    }
+}
+
