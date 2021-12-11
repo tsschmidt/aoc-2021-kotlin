d98c763ce4169ecffef16d3733c6535deed6e4e1
Travis Schmidt <travis.schmidt@gmail.com>
2021-12-10 23:06:48 -0800

    Day 11 solution refactor

--- src/day11/Day11.kt
+++ src/day11/Day11.kt
@@ -16,43 +16,24 @@ fun main() {
        if (input[y][x] > 9) {
            flashes.add(y to x)
            input[y][x] = 0
-           if (y - 1 > -1 && x - 1 > -1 && y - 1 to x -1 !in flashes ) {
-               input[y - 1][x - 1]++
-           }
-           if (y - 1 > -1 && y - 1 to x !in flashes) {
-               input[y - 1][x]++
-           }
-           if (y - 1 > -1 && x + 1 < input[y].size && y - 1 to x +1 !in flashes) {
-               input[y - 1][x + 1]++
-           }
-           if (x - 1 > -1 && y to x - 1 !in flashes) {
-               input[y][x -1]++
-           }
-           if (x + 1 < input[y].size && y to x + 1 !in flashes) {
-               input[y][x + 1]++
-           }
-           if (y + 1 < input.size && x - 1 > -1 && y + 1 to x - 1 !in flashes) {
-               input[y + 1][x - 1]++
-           }
-           if (y + 1 < input.size && y + 1 to x !in flashes) {
-               input[y + 1][x]++
-           }
-           if (y + 1 < input.size && x + 1 < input[y].size && y + 1 to x + 1 !in flashes) {
-               input[y + 1][x + 1]++
+           for(y2 in y - 1..y + 1) {
+               for (x2 in x - 1..x + 1) {
+                   if (y2 in 0..9 && x2 in 0..9 && y2 to x2 !in flashes) {
+                       input[y2][x2]++
+                   }
+               }
            }
            return flash(input, if(y - 1 > -1) y - 1 else 0, if (x - 1 > -1) x - 1 else 0, flashes)
        }
-       if (x + 1 < input[y].size) {
-           return flash(input, y, x + 1, flashes)
-       } else if (x + 1 == input[y].size && y + 1 < input.size) {
-           return flash(input, y + 1, 0, flashes)
+       return when {
+           x + 1 < input[y].size -> flash(input, y, x + 1, flashes)
+           x + 1 == input[y].size && y + 1 < input.size ->  flash(input, y + 1, 0, flashes)
+           else -> flashes
        }
-       return flashes
     }
 
     fun part1(input: MutableList<MutableList<Int>>): Int {
         var flashes = 0
-        input.forEach(::println)
         repeat(100) {
             increase(input)
             flashes += flash(input, 0, 0, mutableListOf()).size
